faf4509f5d45dbffa04dc566612dc4af2bcdd38a
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2025-11-28 11:59:32 -0500

    feat: get started / introduction added

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -11,32 +11,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Open Source',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Free, open-source technologies built with and for the community.
+        Foster trust through transparent, collaborative development.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Accessible',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Empower everyone with intuitive, user-friendly
+        tools that work directly in the browser.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Open Standards',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Eliminate proprietary barriers and promote interoperability 
+        through open standards and non-proprietary formats.
       </>
     ),
   },
